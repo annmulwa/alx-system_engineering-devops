b26b00b91d13ccedbf2178a047bd74d08940b2fb
annmulwa <annmulwa2000@gmail.com>
2024-05-07 00:44:11 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+function that queries the Reddit API and
+returns the number of subscribers for a given subreddit
+'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    returns the number of subscribers for a given subreddit
+    '''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
